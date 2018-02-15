f7906dc92962a2a81a5744ef45303e388ed10e21
moinak <gmoinak50689@gmail.com>
2018-02-16 04:24:29 +0530

    first commit

---
+++ helloworldcontract.sol
@@ -0,0 +1,25 @@
+pragma solidity ^0.4.0;
+
+
+contract HelloWorldContract {
+    string word = "Hello World";
+    address issuer;
+
+    function helloworld(){
+        issuer = msg.sender;
+  }
+
+    function getword() constant returns(string){
+        return word;
+
+  }
+
+    function setword(string newWord) returns(string) {
+        if (issuer != msg.sender) {
+            return "This is not the creator!!";
+        }else {
+            word = newWord;
+            return "this is the creator";
+      }
+    }
+}
